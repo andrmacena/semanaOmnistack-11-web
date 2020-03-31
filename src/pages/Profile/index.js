aa7dadfd2d95b1a142f022562486e736acec6956
André Macena <andrmacena_@outlook.com>
2020-03-30 22:51:36 -0300

    Alterado chamado do metodo de delecao, bug corrigido

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -71,7 +71,7 @@ function Profile() {
                   <strong>Valor:</strong>
                   <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-                  <button onClick={() => { handleDeleteIncident(incident.id) }} type="button">
+                  <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                      <FiTrash2 size={20} color="#a8a8b3" />
                   </button>
                </li>
