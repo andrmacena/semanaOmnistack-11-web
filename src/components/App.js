0ea96a23de6a8d2ecbb81135ee9633d91e33f498
André Macena <andrmacena_@outlook.com>
2021-02-05 21:09:23 -0300

    Adicionado componente responsável pelos alertas da aplicacao

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Routes from '../routes'
 import  '../../src/global.css'
+import { positions, Provider } from "react-alert"
+import AlertTemplate from "react-alert-template-basic"
+
+const options = {
+   timeout: 5000,
+   position: positions.BOTTOM_CENTER
+ }
+ 
 
 function App() {
    return (
-      <Routes />
+      <Provider template={AlertTemplate} {...options}>
+         <Routes />
+      </Provider>
    )
 }
 
