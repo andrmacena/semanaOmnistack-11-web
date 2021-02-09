895db9f83a52839c7e8a18830b4c37c1846c261d
André Macena <andrmacena_@outlook.com>
2021-02-08 22:57:23 -0300

    Adicionada nova rota para a página "Sobre mim"

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Logon from './pages/Logon/index'
 import Register from './pages/Register/index'
 import Profile from './pages/Profile/index'
 import NewIncident from './pages/NewIncident/index'
+import AboutMe from './pages/AboutMe/index'
 
 export default function Routes() {
    return (
@@ -14,6 +15,7 @@ export default function Routes() {
             <Route path="/register" component={Register} /> 
             <Route path="/profile" component={Profile} />  
             <Route path="/incidents/new" component={NewIncident} />  
+            <Route path="/aboutme" component={AboutMe} />  
          </Switch>
       </BrowserRouter>
    )
