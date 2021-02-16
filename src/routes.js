3d5edab5abd6ff2a7ed8fc86052170ceee7fb0e5
André Macena <andrmacena_@outlook.com>
2021-02-15 22:55:39 -0300

    Alterado algumas rotas e adicionado rotas novas para as novas paginas

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Logon from './pages/Logon/index'
 import Register from './pages/Register/index'
-import Profile from './pages/Profile/index'
+import Home from './pages/Home/index'
 import NewIncident from './pages/NewIncident/index'
+import AddNewImage from './pages/AddNewImage/index'
 import AboutMe from './pages/AboutMe/index'
 
 export default function Routes() {
@@ -13,9 +14,10 @@ export default function Routes() {
          <Switch>
             <Route path="/" exact component={Logon} />
             <Route path="/register" component={Register} /> 
-            <Route path="/profile" component={Profile} />  
+            <Route path="/home" component={Home} />  
             <Route path="/incidents/new" component={NewIncident} />  
-            <Route path="/aboutme" component={AboutMe} />  
+            <Route path="/profile" component={AboutMe} />  
+            <Route path="/images/new" component={AddNewImage} />  
          </Switch>
       </BrowserRouter>
    )
