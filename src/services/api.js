df9ae7d5d4a71b2b9ff2a3753947bc28aa702e5c
André Macena <andrmacena_@outlook.com>
2021-03-03 21:41:53 -0300

    feat: Adicionado novo endereço da api, hospedada no azure

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-   baseURL: 'https://bethehero-backend-11.herokuapp.com'
+   baseURL: 'https://besupportive.azurewebsites.net'
 })
 
 export default api
